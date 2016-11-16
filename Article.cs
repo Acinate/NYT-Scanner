858dfbb7d4201146d99e7742a478b1e3f6b14e07
Acinate <Acinate@hotmail.com>
2016-11-16 16:48:52 -0500

    Major updates and refactoring

---
+++ Article.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KeywordPart2
+{
+    public class Article
+    {
+        public List<Sentence> SentenceList { get; set; }
+        public List<Word> WordList { get; set; }
+        public List<Keyword> KeywordList { get; set; }
+        public string raw_text { get; set; }
+        public int average_score { get; set; }
+        public string summary { get; set; }
+    }
+
+    public class Sentence
+    {
+        public string contents { get; set; }
+        public int score { get; set; }
+        public static List<string> FixSentences(List<string> sentences)
+        {
+            for (int i = 0; i < sentences.Count; i++)
+            {
+                // REMOVE SPACE AT START OF SENTENCE
+                try
+                {
+                    if (sentences[i].StartsWith(" "))
+                    {
+                        sentences[i] = sentences[i].Remove(0, 1);
+                    }
+
+                    // FIX NAME PREFIX
+                    if (sentences[i].Contains("Mr.") || sentences[i].Contains("Mrs.") || sentences[i].Contains("Dr.") || sentences[i].Contains("Prof."))
+                    {
+                        sentences[i] += sentences[i + 1];
+                        sentences.RemoveAt(i + 1);
+                    }
+
+                    // DETECTS ABBREVIATED WORDS (CAN BE SCALED => ADJUST LENGTH BASED ON NUMBER OF '.')
+                    if (sentences[i].Contains(".") && sentences[i].Length < 5)
+                    {
+                        sentences[i + 1] = sentences[i + 1].Insert(0, sentences[i]);
+                        sentences.RemoveAt(i);
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            return sentences;
+        }
+    }
+
+    public class Word
+    {
+        public string contents { get; set; }
+        public bool ContainsPunctuation(char c)
+        {
+            if (c == ' ' || c == '.' || c == ',' || c == '-' || c == '_' || c == ';' || c == ':')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool isJunkWord(string keyword)
+        {
+            keyword = keyword.ToLower();
+            if (keyword == "the" || keyword == "that" || keyword == "or" || keyword == "is" || keyword == "in" || keyword == "to" || keyword == "a" || keyword == "an" || keyword == "has" || keyword == "of" || keyword == "its" || keyword == "and" || keyword == "but" || keyword == "as" || keyword == "it" || keyword == "be" || keyword == "which" ||
+                keyword == "there" || keyword == "for" || keyword == "he" || keyword == "his" || keyword == "hers" || keyword == "her" || keyword == "by" || keyword == "on" || keyword == "than" || keyword == "i" || keyword == "with" || keyword == "us" || keyword == "our" || keyword == "you" || keyword == "can" || keyword == "")
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public class Keyword
+    {
+        public int count { get; set; }
+        public string contents { get; set; }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,185 +7,113 @@ namespace KeywordPart2
 {
     class Program
     {
+        private static Article article = new Article() { SentenceList = new List<Sentence>(), KeywordList = new List<Keyword>(), WordList = new List<Word>() };
+        private static Sentence sentence = new Sentence();
+        private static Keyword keyword = new Keyword();
+        private static List<string> used_words = new List<string>();
+        private static Word word = new Word();
+
         static void Main(string[] args)
         {
-            // READS LINES FROM TEXT FILE, SAVES TO STRING
-            string paragraphText = "";
             foreach (var v in Reader.ReadFromFile())
             {
-                paragraphText += v;
+                article.raw_text += v;
             }
 
-            // DEBUG
-            Console.WriteLine(paragraphText);
-
-            // BREAKS PARAGRAPH TEXT STRING INTO LIST OF SENTENCES
-            List<string> sentences = new List<string>();
-            string sentence = "";
-            for (int i = 0; i < paragraphText.Length; i++)
+            // Prints extracted text
+            Console.WriteLine(article.raw_text);
+            
+            for (int i = 0; i < article.raw_text.Length - 1; ++i)
             {
-                if (paragraphText[i] == '.' && paragraphText[i + 1] == ' ')
+                // If we detect the end of a sentence
+                if (article.raw_text[i] == '.' && article.raw_text[i+1] == ' ')
                 {
-                    sentence += paragraphText[i];
-                    sentences.Add(sentence);
-                    sentence = "";
-                } else
+                    // Save the sentence to a list
+                    // Clear the sentence object
+                    article.SentenceList.Add(new Sentence { contents = sentence.contents + '.' });
+                    sentence.contents = "";
+                }
+                else
                 {
-                    sentence += paragraphText[i];
+                    // Build the sentence
+                    sentence.contents += article.raw_text[i];
                 }
             }
 
-            // FIXES SENTENCES
-            sentences = Word.FixSentences(sentences);
-
-            // DEBUG
-            foreach (var s in sentences)
+            // Prints each gathered sentence
+            foreach(var s in article.SentenceList)
             {
-                // Console.WriteLine(s + "\n");
+                Console.WriteLine(s.contents);
             }
 
-            // GRABS KEYWORDS -- DOES NOT ADD JUNK KEYWORDS
-            List<string> paragraphs = new List<string>();
-            List<string> keywords = new List<string>();
-            string keyword = "";
-            foreach (string s in sentences)
+            for (int i = 0; i < article.raw_text.Length - 1; ++i)
             {
-                foreach (char c in s)
+                if (word.ContainsPunctuation(article.raw_text[i]))
                 {
-                    if (!char.IsWhiteSpace(c) && Word.hasPunctionation(c))
+                    if (!used_words.Contains(word.contents) && !word.isJunkWord(word.contents))
                     {
-                        keyword += c;
+                        used_words.Add(word.contents);
+                        article.KeywordList.Add(new Keyword { contents = word.contents });
                     }
-                    else
-                    {
-                        if (keyword != "" && !keyword.Contains('.'))
-                        {
-                            if (keyword.Contains("’s"))
-                                keyword = Word.removeApostrophy(keyword);
-                            if (!Word.isJunkWord(keyword))
-                            {
-                                keywords.Add(keyword);
-                            }
-                        }
-                        keyword = "";
-                    }
+                    article.WordList.Add(new Word { contents = word.contents });
+                    word.contents = "";
+                }
+                else
+                {
+                    // Build the word
+                    word.contents += article.raw_text[i];
                 }
             }
-
-            // COUNTS THE OCCURENCES OF EACH KEYWORD
-            var numKeywords = new List<int>();
-            foreach (string s in keywords)
+            
+            foreach (var key in article.KeywordList)
             {
-
-                int count = 0;
-                foreach (string z in keywords)
+                foreach (var word in article.WordList)
                 {
-                    if (z == s) 
+                    if (word.contents == key.contents)
                     {
-
-                        count++;
-                    }
-                }
-                numKeywords.Add(count);
-            }
-
-            // CALCULATIONS FOR DETECTING AVERAGE KEYWORD OCCURENCE
-            int sum = 0, average = 0, average75 = 0;
-            foreach (int i in numKeywords)
-            {
-                sum += numKeywords[i];
-            }
-
-            // AVERAGE KEYWORD OCCURENCE
-            average = sum / (numKeywords.Count() / 2);
-
-            // AVERAGE + AVERAGE / 2 CALCULATION
-            average75 = average;
-
-            // && !keywords.Contains(keyword)
-            List<string> goodKeyWords = new List<string>();
-            List<int> sentenceScore = new List<int>();
-            for (int i = 0; i < sentences.Count; i++)
-            {
-                int score = 0;
-                sentenceScore.Add(score);
-                for (int j = 0; j < numKeywords.Count; j++)
-                {
-                    if (numKeywords[j] >= average75)
-                    {
-                        if (!goodKeyWords.Contains(keywords[j]))
-                            goodKeyWords.Add(keywords[j]);
-                        if (sentences[i].Contains(keywords[j]))
-                        {
-                            sentenceScore[i] += 1;
-                        }
+                        key.count += 1;
                     }
                 }
             }
 
-            // CALCULATIONS FOR DETECTING AVERAGE KEYWORD OCCURENCE
-            foreach (int i in numKeywords)
+            // Prints each gathered keyword
+            foreach (var w in article.KeywordList)
             {
-                sum += i;
+                Console.WriteLine(w.contents + " : " + w.count);
             }
 
-            // AVERAGE KEYWORD OCCURENCE
-            average = sum / (numKeywords.Count() / 8);
-
-            // AVERAGE + AVERAGE / 2 CALCULATION
-            average75 = average + (average / 3);
-
-            string result = "";
-            List<string> summarySentences = new List<string>();
-            for (int i = 0; i < sentenceScore.Count; i++)
+            foreach (var s in article.SentenceList)
             {
-                if (sentenceScore[i] > average75)
+                foreach (var k in article.KeywordList)
                 {
-                    summarySentences.Add(sentences[i]);
+                    if (s.contents.Contains(k.contents))
+                    {
+                        s.score += k.count;
+                    }
+                }
+
+                article.average_score += s.score;
+            }
+
+            article.average_score = article.average_score / article.SentenceList.Count;
+            
+            foreach (var s in article.SentenceList)
+            {
+                if (s.score > article.average_score)
+                {
+                    article.summary += s.contents;
                 }
             }
 
-            foreach (string ss in summarySentences)
-            {
-                result += ss + " ";
-            }
-
-            result += "\n\nKeywords Used:\n";
-
-            foreach (string gkw in goodKeyWords)
-            {
-                result += gkw + "\n";
-            }
-
-            Console.WriteLine("\n\nSummary:\n" + result);
-            Reader.WriteListToText("", "tesla_summary", result);
+            Console.WriteLine("\n\nSummary:\n" + article.summary);
+            Reader.WriteListToText("", "tesla_summary", article.summary);
             Console.ReadLine();
         }
     }
 
-    class Word
+    class Word2
     {
-        public static string keyword { get; set; }
-
-        //public bool isKeyword(List<String> Sentences)
-        //{
-        //    for (int i = 0; i < Sentences.Count(); i++)
-        //    {
-        //        string sent = "";
-        //    }
-        //}
-
-        public string contents { get; set; }
-
-        public static bool isJunkWord(string keyword)
-        {
-            keyword = keyword.ToLower();
-            if (keyword == "the" || keyword == "that" || keyword == "or" || keyword == "is" || keyword == "in" || keyword == "to" || keyword == "a" || keyword == "an" || keyword == "has" || keyword == "of" || keyword == "its" || keyword == "and" || keyword == "but" || keyword == "as" || keyword == "it" || keyword == "be" || keyword == "which" ||
-                keyword == "there" || keyword == "for" || keyword == "he" || keyword == "his" || keyword == "hers" || keyword == "her" || keyword == "by" || keyword == "on" || keyword == "than" || keyword == "i")
-                return true;
-            else
-                return false;
-        }
+        
 
         public static string removeApostrophy(string keyword)
         {
@@ -205,32 +133,34 @@ namespace KeywordPart2
             for (int i = 0; i < sentences.Count; i++)
             {
                 // REMOVE SPACE AT START OF SENTENCE
-                if (sentences[i].StartsWith(" "))
+                try
                 {
-                    sentences[i] = sentences[i].Remove(0,1);
-                }
+                    if (sentences[i].StartsWith(" "))
+                    {
+                        sentences[i] = sentences[i].Remove(0, 1);
+                    }
 
-                // FIX NAME PREFIX
-                if (sentences[i].Contains("Mr.") || sentences[i].Contains("Mrs.") || sentences[i].Contains("Dr.") || sentences[i].Contains("Prof."))
-                {
-                    sentences[i] += sentences[i + 1];
-                    sentences.RemoveAt(i + 1);
-                }
+                    // FIX NAME PREFIX
+                    if (sentences[i].Contains("Mr.") || sentences[i].Contains("Mrs.") || sentences[i].Contains("Dr.") || sentences[i].Contains("Prof."))
+                    {
+                        sentences[i] += sentences[i + 1];
+                        sentences.RemoveAt(i + 1);
+                    }
 
-                // DETECTS ABBREVIATED WORDS (CAN BE SCALED => ADJUST LENGTH BASED ON NUMBER OF '.')
-                if (sentences[i].Contains(".") && sentences[i].Length < 5)
+                    // DETECTS ABBREVIATED WORDS (CAN BE SCALED => ADJUST LENGTH BASED ON NUMBER OF '.')
+                    if (sentences[i].Contains(".") && sentences[i].Length < 5)
+                    {
+                        sentences[i + 1] = sentences[i + 1].Insert(0, sentences[i]);
+                        sentences.RemoveAt(i);
+                    }
+                }
+                catch (Exception)
                 {
-                    sentences[i + 1] = sentences[i + 1].Insert(0, sentences[i]);
-                    sentences.RemoveAt(i);
+
                 }
             }
             return sentences;
         }
-    }
-
-    class Sentence
-    {
-        public string contents { get; set; }
     }
 
     class Reader
